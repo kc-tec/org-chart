021b1a7708ed28bcb5e38c1b7e5ef884e43b4736
Kakratana <39192742+Kakratana@users.noreply.github.com>
2026-05-21 14:39:19 +0700

    commit

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ var CONFIG = {
   "boxHeight": 50,
   "title": {
     "color": "#05668d",
-    "text": "KCTEC Organization Chart 2026"
+    "text": "KCTEC Org Chart 2026"
   },
   "information": "Do you see something wrong? Please drop a <em>mail</em> to <a href=\"mailto:kak.ratana@gmail.com?Subject=Hello%20again\" target=\"_top\">kak.ratana@gmail.com</a>",
   "photoUrl": {

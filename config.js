ace4b1216b70023e2370fb5519693cb0838e383b
Kakratana <39192742+Kakratana@users.noreply.github.com>
2026-05-26 12:22:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,6 @@ var CONFIG = {
   "boxHeight": 50,
   "title": {
     "color": "#0068ce",
-	"background-color": "#0068ce",
    "text": "KCTEC"
   },
   "information": "Do you see something wrong? Please drop a <em>mail</em> to <a href=\"mailto:kak.ratana@gmail.com?Subject=Hello%20again\" target=\"_top\">kak.ratana@gmail.com</a>",
